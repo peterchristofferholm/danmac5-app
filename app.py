2251aa4fc5f79c620612a129e67b55a084bb7d95
peterchristofferholm <petchdk@gmail.com>
2022-07-27 11:35:38 +0200

    update app.py

--- app.py
+++ app.py
@@ -121,5 +121,5 @@ app.layout = dbc.Container([
     storage
 ])
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+if __name__ == "__main__":
+    app.run_server(host="0.0.0.0", port=80)
